020b6f6d06dc50ee7433617ea6bb21b8fd7e5db2
Sebastian Flor <sebastian.flor@networkteam.com>
2017-01-20 22:45:27 +0100

    Adds demos and first readme

--- Demo/app/Demo2.js
+++ Demo/app/Demo2.js
@@ -5,22 +5,71 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
+  Image,
   TouchableHighlight,
   Text,
   View
 } from 'react-native';
+import ZoomBox from 'react-native-zoombox';
 
 class Demo2 extends Component {
   _goBack() {
     this.props.navigator.pop();
   }
 
+  _customHeader(closeModal) {
+    return (
+        <TouchableHighlight onPress = {closeModal}>
+          <Text style = {styles.closeButton}>Close</Text>
+        </TouchableHighlight>
+    )
+  }
+
+  _customContent() {
+    return(
+      <View style = {[styles.container, {alignItems: 'center', marginTop: 0}]}>
+        <Text style = {styles.headline}>The content of the modal differs from the touchable content</Text>
+      </View>
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Demo 2</Text>
+        <Text style = {styles.headline}>Swipe to close</Text>
+        <ZoomBox
+          style = {styles.zoomBox}
+          swipeToClose = {true}>
+          <Image
+            source = {{uri: 'https://placehold.it/800x600'}}
+            resizeMode = 'contain'
+            style = {styles.contain} />
+        </ZoomBox>
+
+        <Text style = {styles.headline}>Custom header</Text>
+        <ZoomBox
+          style = {styles.zoomBox}
+          customHeader = {this._customHeader}>
+          <Image
+            source = {{uri: 'https://placehold.it/800x600'}}
+            resizeMode = 'contain'
+            style = {styles.contain} />
+        </ZoomBox>
+
+        <Text style = {styles.headline}>Custom content</Text>
+        <ZoomBox
+          style = {styles.zoomBox}
+          customContent = {this._customContent}>
+          <Image
+            source = {{uri: 'https://placehold.it/800x600'}}
+            resizeMode = 'contain'
+            style = {styles.contain} />
+        </ZoomBox>
+
         <TouchableHighlight
-          onPress = {() => this._goBack()}>
+          onPress = {() => this._goBack()}
+          underlayColor = '#fff'
+          style = {{flex: 0.5, alignSelf: 'center'}}>
             <Text>Go Back</Text>
           </TouchableHighlight>
       </View>
@@ -31,20 +80,38 @@ class Demo2 extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    justifyContent: 'center',
+    marginTop: 20,
+    padding: 20
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+
+  headline: {
+    fontSize: 16
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+
+  zoomBox: {
+    flex: 1,
+    width: 100,
+    alignSelf: 'center'
   },
+
+  contain: {
+    flex: 1
+  },
+
+  closeButton: {
+  fontSize: 16,
+  color: 'red',
+  lineHeight: 40,
+  textAlign: 'center',
+  alignSelf: 'flex-end',
+  width: 60,
+  marginRight: 20,
+  marginTop: 10,
+  borderWidth: 1,
+  borderColor: 'red'
+}
 });
 
 export default Demo2;
--- Demo/app/Demo3.js
+++ Demo/app/Demo3.js
@@ -6,21 +6,40 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   TouchableHighlight,
+  Animated,
   Text,
   View
 } from 'react-native';
+
+import ZoomBox from 'react-native-zoombox'
 
 class Demo3 extends Component {
   _goBack() {
     this.props.navigator.pop();
   }
 
+  _customAnimation(startValue, endValue) {
+    return(Animated.timing(
+      startValue,
+      {toValue: endValue, duration: 1000}
+    ))
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Demo 3</Text>
+        <Text style = {styles.headline}>Custom Animation</Text>
+        <ZoomBox style = {styles.zoomBox}
+                  customAnimation = {this._customAnimation}>
+          <View style = {{flex: 1, justifyContent: 'center', alignItems: 'center', marginTop: 0}}>
+            <Text style = {{fontSize: 16, color: 'red', backgroundColor: '#ccc', padding: 10}}>Text Content in a modal</Text>
+          </View>
+        </ZoomBox>
+
         <TouchableHighlight
-          onPress = {() => this._goBack()}>
+          underlayColor = '#fff'
+          onPress = {() => this._goBack()}
+          style = {{flex: 0.5, alignSelf: 'center'}}>
             <Text>Go Back</Text>
           </TouchableHighlight>
       </View>
@@ -31,19 +50,24 @@ class Demo3 extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    justifyContent: 'center',
+    marginTop: 20,
+    padding: 20
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+
+  headline: {
+    fontSize: 16
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+
+  zoomBox: {
+    flex: 1,
+    width: 100,
+    alignSelf: 'center'
+  },
+
+  contain: {
+    flex: 1
   },
 });
 
